df881daad837156ac940dfe831f27e395414d812
Hakkani Mim <mimhakkani4g@gmail.com>
2021-11-15 19:23:13 +0600

    state is update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 const port = 5000
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6azwl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://BycycleStore:O2CVQMYKx2jgTGeF@cluster0.6azwl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -15,6 +15,7 @@ client.connect(err => {
     const addservecollection = client.db("Bycyle").collection("addservices");
     const ordercollection = client.db("Bycyle").collection("order");
     const reviewscollection =client.db("Bycyle").collection("review");
+    const usersCollection = client.db("Bycyle").collection('users');
   
 
  app.post('/addservices',async(req,res)=>{
@@ -41,7 +42,9 @@ client.connect(err => {
  app.post('/ordernow',async(req,res)=>{
 
   const query =req.body
+  
   const result = await ordercollection.insertOne(query);
+  console.log(result);
    res.send(result);
 
  })
@@ -86,6 +89,64 @@ client.connect(err => {
   res.send(result)
  })
 
+
+ //manage all orders delete method 
+
+ app.delete('/manageallorderdelete/:id',async(req,res)=>{
+   const query =req.params.id;
+   const result = await ordercollection.deleteOne({_id:ObjectId(query)})
+   res.send(result) 
+ })
+
+ //status update 
+
+ app.put('/updatestatus/:id',async(req,res)=>{
+  const id=req.params.id
+  const filter={_id:ObjectId(id)}
+  const updateStatus = {
+      $set: {
+        status:"Approved"
+      }
+    };
+    const result=await ordercollection.updateOne(filter,updateStatus)
+    res.send(result)
+})
+
+app.get('/users/:email', async (req, res) => {
+  const email = req.params.email;
+  const query = { email: email };
+  const user = await usersCollection.findOne(query);
+  let isAdmin = false;
+  if (user?.role === 'admin') {
+      isAdmin = true;
+  }
+  res.json({ admin: isAdmin });
+})
+
+app.post('/users', async (req, res) => {
+  const user = req.body;
+  const result = await usersCollection.insertOne(user);
+  console.log(result);
+  res.json(result);
+});
+
+app.put('/users', async (req, res) => {
+  const user = req.body;
+  const filter = { email: user.email };
+  const options = { upsert: true };
+  const updateDoc = { $set: user };
+  const result = await usersCollection.updateOne(filter, updateDoc, options);
+  res.json(result);
+});
+
+app.put('/users/admin', async (req, res) => {
+      const user = req.body;
+          const filter = { email: user.email };
+          const updateDoc = { $set: { role: 'admin' } };
+          const result = await usersCollection.updateOne(filter, updateDoc);
+          res.json(result);
+
+})
 
 
 
